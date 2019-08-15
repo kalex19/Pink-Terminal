d2452e7b7b7b3dfa8ec1baa415a95fbac7647d89
Katherine Lewis <kale8432@colorado.edu>
2019-08-14 22:43:31 -0400

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Header/>
+      <About/>
+      <Portfolio/>
+      <Media/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
