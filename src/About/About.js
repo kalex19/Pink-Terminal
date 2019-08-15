cb008867d03357725ab4b61610f2877061d0bbea
Katherine Lewis <kale8432@colorado.edu>
2019-08-15 15:35:34 -0400

    portfolio image

--- src/About/About.js
+++ src/About/About.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
+import "./About.css";
 
 export default function About() {
   return (
     <section>
-      <article>
-        <img class="profileImg" src="./uby-yanes-hNmEo1IllUg-unsplash.jpg" alt="About Katie"></img>
+      <article className="profile">
       </article>
-      <article>
-        <h3>About Katie Lewis</h3>
-        <p>My bio which is all about me and how I am uniquely similar to everyone else. </p>
+      <article class="content">
+        <h3 className="aboutTitle">About Katie Lewis</h3>
+        <p className="bioContent">My bio which is all about me and how I am uniquely similar to everyone else. </p>
       </article>
     </section>
   )
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,9 +5,9 @@ export default function Header() {
   return (
     <header>
       <main className="gallery">
-        <h2>Dear Kate,</h2>
-        <h3>Be unapologetically proud of who you are!</h3>
-        <p class="icon">⧨</p>
+        <h2 className="header">Dear Kate,</h2>
+        <h3 className="message">Be unapologetically proud of who you are!</h3>
+        <p className="icon">⧨</p>
       </main>
     </header>
   )
