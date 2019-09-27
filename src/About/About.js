bddfdd7931589a828ba96760ee2631c67af1603d
Katherine Lewis <kale8432@colorado.edu>
2019-09-27 08:19:14 -0600

    CLeaned up header icon style

--- src/About/About.js
+++ src/About/About.js
@@ -3,11 +3,11 @@ import "./About.css";
 
 export default function About() {
   return (
-    <section>
-      <article id="about" className="profile">
+    <section id="about">
+      <article className="profile">
       </article>
       <article class="content">
-        <h3 className="aboutTitle">About Katie Lewis</h3>
+        <h3  className="aboutTitle">About Katie Lewis</h3>
         <p className="bioContent">My bio which is all about me and how I am uniquely similar to everyone else. </p>
       </article>
       <article className="socialMedia">
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import "./Header.css";
+import { Slide } from 'react-slideshow-image';
 
 export default function Header() {
+
   return (
     <header>
       <main className="gallery">
         <h2 className="header">Dear Kate,</h2>
         <h3 className="message">Be unapologetically proud of who you are!</h3>
-        <p className="icon">⧨</p>
+        <p  className="icon"><a className="iconStyle">◮</a></p>
       </main>
     </header>
   )
